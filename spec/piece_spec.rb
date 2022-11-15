2236b83d52ecbc055c45242f8e9470714d5918fc
Olly Seber <olly_s@hotmail.co.uk>
2022-11-15 13:14:45 -0800

    Update Piece factory test to expect a class method

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -5,10 +5,10 @@ require_relative "../lib/piece"
 describe Piece do
   subject(:piece) { described_class.new }
 
-  describe "#create_piece" do
-    context "when the type is NONE" do
+  describe "#self.create_piece" do
+    context "when no type is given" do
       it "returns a Piece object" do
-        expect(piece.create_piece).to be_a(Piece)
+        expect(Piece.create_piece).to be_a(Piece)
       end
     end
   end
