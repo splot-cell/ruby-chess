cd5808ea753cfa83c75aa71276fd336679fdea91
Olly Seber <olly_s@hotmail.co.uk>
2022-11-15 13:23:56 -0800

    Test piece factory creates object of type Pawn

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -3,8 +3,6 @@
 require_relative "../lib/piece"
 
 describe Piece do
-  subject(:piece) { described_class.new }
-
   describe "#self.create_piece" do
     context "when no type is given" do
       it "returns a Piece object" do
@@ -17,6 +15,12 @@ describe Piece do
 
       it "returns a Piece object of color nil" do
         expect(Piece.create_piece.color).to be(nil)
+      end
+    end
+
+    context "when type is PAWN" do
+      it "returns an object of type Pawn" do
+        expect(Piece.create_piece(PieceType::PAWN)).to be_a(Pawn)
       end
     end
   end
