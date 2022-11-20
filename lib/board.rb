8054eca1b0068ef3871d471cf1b25f115c6920e6
Olly Seber <olly_s@hotmail.co.uk>
2022-11-20 13:13:41 -0800

    Implement Board#restore_position

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,27 @@ class Board
     @data = position.nil? ? Array.new(64, piece_class.new) : position
   end
 
+  def restore_position(fen_str)
+    pos = fen_str.split
+    piece_placement = pos[0]
+    active_color = pos[1]
+    castling = pos[2]
+    en_passant = pos[3]
+    half_move_clk = pos[4]
+    full_move_num = pos[5]
+    place_pieces(piece_placement)
+  end
+
+  def place_pieces(str)
+    @data.clear
+    str.split("").each do |char|
+      if char =~ /[kqrbnp]/i
+        color = char.match?(/[A-Z]/) ? PieceColor::WHITE : PieceColor::BLACK
+        @data << Piece.create(Piece.char_to_type(char), color)
+      elsif char =~ /[0-8]/
+        char.to_i.times { @data << Piece.create }
+      end
+    end
+  end
+
 end
--- lib/piece.rb
+++ lib/piece.rb
@@ -45,4 +45,15 @@ class Piece
   def self.type
     NONE
   end
+
+  def self.char_to_type(char)
+    {
+      "k" => KING,
+      "q" => QUEEN,
+      "r" => ROOK,
+      "b" => BISHOP,
+      "n" => KNIGHT,
+      "p" => PAWN
+    }[char.downcase]
+  end
 end
