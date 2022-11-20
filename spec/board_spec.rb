fb8af1797fbbf4ac5dfb60ee39d583efc8ae6ec1
Olly Seber <olly_s@hotmail.co.uk>
2022-11-20 10:34:04 -0800

    Improve testing of @data creation in #initialize

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../lib/board"
+require_relative "../lib/piece"
 
 describe Board do
   describe "#initialize" do
@@ -10,14 +11,13 @@ describe Board do
         expect(board.instance_variable_get(:@data)).to be_an(Array)
       end
 
-      it "creates an array of Pieces" do
-        expect(board.instance_variable_get(:@data)[0]).to be_a(Piece)
-      end
-
       it "creates an array of length 64" do
         expect(board.instance_variable_get(:@data).length).to eq(64)
       end
+
+      it "creates an array of Pieces" do
+        expect(board.instance_variable_get(:@data)).to all(be_a(Piece))
+      end
     end
   end
-  let(:data) {  }
 end
