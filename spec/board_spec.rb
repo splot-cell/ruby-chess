2ca4dc176fc9ef42844585202c214a8bc4884a28
Olly Seber <olly_s@hotmail.co.uk>
2022-11-20 10:21:44 -0800

    Write tests for #initialize with no arguments

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,5 +3,21 @@
 require_relative "../lib/board"
 
 describe Board do
-  subject(:board) { described_class.new }
+  describe "#initialize" do
+    subject(:board) { described_class.new }
+    context "when no arguments are given" do
+      it "creates an array" do
+        expect(board.instance_variable_get(:@data)).to be_an(Array)
+      end
+
+      it "creates an array of Pieces" do
+        expect(board.instance_variable_get(:@data)[0]).to be_a(Piece)
+      end
+
+      it "creates an array of length 64" do
+        expect(board.instance_variable_get(:@data).length).to eq(64)
+      end
+    end
+  end
+  let(:data) {  }
 end
