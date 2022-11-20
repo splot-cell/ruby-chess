8d269823faa27a460604b1b2e8ac1321ac0b9a94
Olly Seber <olly_s@hotmail.co.uk>
2022-11-20 12:36:31 -0800

    Create tests for #restore_position

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -30,4 +30,38 @@ describe Board do
       end
     end
   end
+
+  describe "#restore_position" do
+    context "when the position contains only a white king in square a8" do
+      let(:position1) { "K7/8/8/8/8/8/8/8 w - - 0 1" }
+      it "populates @data[0] with a Piece of type KING" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0] }.to be_a(King)
+      end
+
+      it "populates @data[0] with a Piece of color WHITE" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0].color }.to be(PieceColor::WHITE)
+      end
+
+      it "populates @data with 64 objects" do
+        board.restore_position(position1)
+        expect(board.instance_variable_get(:@data).length).to eq(64)
+      end
+
+      it "populates @data[1] with a Piece" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[1] }.to be_a(Piece)
+      end
+
+      it "populates @data[1] with a Piece of type NONE" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[1].type }.to be(PieceType::None)
+      end
+
+      it "populates @data[63] with a Piece of type NONE" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[63].type }.to be(PieceType::None)
+      end
+
+      it "populates @data[44] with a Piece of type NONE" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[44].type }.to be(PieceType::None)
+      end
+    end
+  end
 end
