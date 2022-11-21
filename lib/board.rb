88f818c4c6535dd7cb2e2c8759d8eae24f4cc736
Olly Seber <olly_s@hotmail.co.uk>
2022-11-21 09:08:10 -0800

    Refactor Board@data to be a 2-D array

--- lib/board.rb
+++ lib/board.rb
@@ -6,8 +6,8 @@ require_relative "fen"
 class Board
   include FEN
 
-  def initialize(position = nil, piece_class = Piece)
-    @data = position.nil? ? Array.new(64, piece_class.new) : position
+  def initialize(piece_class = Piece)
+    @data = Array.new(8) { Array.new(8, piece_class.new) }
   end
 
   def restore_position(fen_str)
@@ -17,14 +17,16 @@ class Board
 
   def place_pieces(fen_str)
     @data.clear
-    fen_str.split("").each do |char|
-      next if char == next_rank
-
-      if empty_square?(char)
-        char.to_i.times { @data << Piece.create }
-      else
-        @data << Piece.create(piece_type_from(char), piece_color_from(char))
+    fen_str.split("/").each do |r|
+      rank = []
+      r.split("").each do |char|
+        if empty_square?(char)
+          char.to_i.times { rank << Piece.create }
+        else
+          rank << Piece.create(piece_type_from(char), piece_color_from(char))
+        end
       end
+      @data << rank
     end
   end
 
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,27 +6,29 @@ require_relative "../lib/piece"
 describe Board do
   subject(:board) { described_class.new }
   describe "#initialize" do
+    matcher :be_length_8 do
+      match { |arr| arr.length == 8 }
+    end
+
     context "when no arguments are given" do
       it "creates an array" do
         expect(board.instance_variable_get(:@data)).to be_an(Array)
       end
 
-      it "creates an array of length 64" do
-        expect(board.instance_variable_get(:@data).length).to eq(64)
+      it "creates an array of length 8" do
+        expect(board.instance_variable_get(:@data)).to be_length_8
       end
 
-      it "creates an array of Pieces" do
-        expect(board.instance_variable_get(:@data)).to all(be_a(Piece))
+      it "creates an array of Arrays" do
+        expect(board.instance_variable_get(:@data)).to all(be_an(Array))
       end
-    end
 
-    context "when a position is passed in" do
-      let(:piece_class) { double("piece_class") }
+      it "creates an array of arrays of length 8" do
+        expect(board.instance_variable_get(:@data)).to all(be_length_8)
+      end
 
-      it "does not create new Piece objects" do
-        position = "..."
-        expect(piece_class).not_to receive(:new)
-        described_class.new(position, piece_class)
+      it "creates an array of arrays of Pieces" do
+        expect(board.instance_variable_get(:@data)).to all(all(be_a(Piece)))
       end
     end
   end
@@ -35,74 +37,74 @@ describe Board do
     context "when the position contains only a white king in square a8" do
       let(:position1) { "K7/8/8/8/8/8/8/8 w - - 0 1" }
 
-      it "populates @data[0] with a Piece of type KING" do
-        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0] }.to be_a(King)
+      it "populates @data[0][0] with a Piece of type KING" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0][0] }.to be_a(King)
       end
 
-      it "populates @data[0] with a Piece of color WHITE" do
-        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0].color }.to be(PieceColor::WHITE)
+      it "populates @data[0][0] with a Piece of color WHITE" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0][0].color }.to be(PieceColor::WHITE)
       end
 
       it "populates @data with 64 objects" do
         board.restore_position(position1)
-        expect(board.instance_variable_get(:@data).length).to eq(64)
+        expect(board.instance_variable_get(:@data).flatten.length).to eq(64)
       end
 
-      it "populates @data[1] with a Piece" do
-        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[1] }.to be_a(Piece)
+      it "populates @data[0][1] with a Piece" do
+        expect { board.restore_position(position1) }.to change { board.instance_variable_get(:@data)[0][1] }.to be_a(Piece)
       end
 
-      it "populates @data[1] with a Piece of type NONE" do
+      it "populates @data[0][1] with a Piece of type NONE" do
         board.restore_position(position1)
-        expect(board.instance_variable_get(:@data)[1].type).to be(PieceType::NONE)
+        expect(board.instance_variable_get(:@data)[0][1].type).to be(PieceType::NONE)
       end
 
-      it "populates @data[63] with a Piece of type NONE" do
+      it "populates @data[7][7] with a Piece of type NONE" do
         board.restore_position(position1)
-        expect(board.instance_variable_get(:@data)[63].type).to be(PieceType::NONE)
+        expect(board.instance_variable_get(:@data)[7][7].type).to be(PieceType::NONE)
       end
 
-      it "populates @data[44] with a Piece of type NONE" do
+      it "populates @data[5][4] with a Piece of type NONE" do
         board.restore_position(position1)
-        expect(board.instance_variable_get(:@data)[44].type).to be(PieceType::NONE)
+        expect(board.instance_variable_get(:@data)[5][4].type).to be(PieceType::NONE)
       end
     end
 
     context "when the position contians a black knight on c4 and a white pawn on h1" do
       let(:position2) { "8/8/8/8/2n5/8/8/7P b - - 2 5" }
 
-      it "populates @data[34] with a Knight" do
+      it "populates @data[4][2] with a Knight" do
         board.restore_position(position2)
-        expect(board.instance_variable_get(:@data)[34]).to be_a(Knight)
+        expect(board.instance_variable_get(:@data)[4][2]).to be_a(Knight)
       end
 
-      it "populates @data[34] with a Piece of color BLACK" do
-        expect { board.restore_position(position2) }.to change { board.instance_variable_get(:@data)[34].color }.to be(PieceColor::BLACK)
+      it "populates @data[4][2] with a Piece of color BLACK" do
+        expect { board.restore_position(position2) }.to change { board.instance_variable_get(:@data)[4][2].color }.to be(PieceColor::BLACK)
       end
 
-      it "populates @data[63] with a Pawn" do
+      it "populates @data[7][7] with a Pawn" do
         board.restore_position(position2)
-        expect(board.instance_variable_get(:@data)[63]).to be_a(Pawn)
+        expect(board.instance_variable_get(:@data)[7][7]).to be_a(Pawn)
 
       end
 
-      it "populates @data[63] with a Piece of color WHITE" do
-        expect { board.restore_position(position2) }.to change { board.instance_variable_get(:@data)[63].color }.to be(PieceColor::WHITE)
+      it "populates @data[7][7] with a Piece of color WHITE" do
+        expect { board.restore_position(position2) }.to change { board.instance_variable_get(:@data)[7][7].color }.to be(PieceColor::WHITE)
       end
 
-      it "populates @data[33] with a Piece of type NONE" do
+      it "populates @data[4][1] with a Piece of type NONE" do
         board.restore_position(position2)
-        expect(board.instance_variable_get(:@data)[33].type).to be(PieceType::NONE)
+        expect(board.instance_variable_get(:@data)[4][1].type).to be(PieceType::NONE)
       end
 
-      it "populates @data[35] with a Piece of type NONE" do
+      it "populates @data[4][3] with a Piece of type NONE" do
         board.restore_position(position2)
-        expect(board.instance_variable_get(:@data)[35].type).to be(PieceType::NONE)
+        expect(board.instance_variable_get(:@data)[4][3].type).to be(PieceType::NONE)
       end
 
-      it "populates @data[2] with a Piece of type NONE" do
+      it "populates @data[0][2] with a Piece of type NONE" do
         board.restore_position(position2)
-        expect(board.instance_variable_get(:@data)[2].type).to be(PieceType::NONE)
+        expect(board.instance_variable_get(:@data)[0][2].type).to be(PieceType::NONE)
       end
     end
   end
