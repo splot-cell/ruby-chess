f4f1d9156f04ae442b10b8752af730d50f15d53a
Olly Seber <olly_s@hotmail.co.uk>
2022-12-05 14:58:18 -0800

    Add movement vectors

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -6,6 +6,25 @@ class King < Piece
   def self.type
     KING
   end
+
+  def move_translations
+    [[-1, -1],
+     [-1, 0],
+     [-1, 1],
+     [0, 1],
+     [1, 1],
+     [1, 0],
+     [1, -1],
+     [0, -1]]
+  end
+
+  def attack_translations
+    move_translations
+  end
+
+  def sliding?
+    false
+  end
 end
 
 Piece.register(King)
