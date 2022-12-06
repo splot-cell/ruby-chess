a8167b16d62ad617b4246c5381172afde893e066
Olly Seber <olly_s@hotmail.co.uk>
2022-12-06 12:44:36 -0800

    Use pawn's rank to detemine move translations

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -3,22 +3,17 @@
 require_relative "../piece"
 
 class Pawn < Piece
-  def initialize(*)
-    super
-    @has_moved = false
-  end
-
   def self.type
     PAWN
   end
 
   def move_translations
     white = [[-1, 0]]
-    white << [-2, 0] unless @has_moved
+    white << [-2, 0] if @position[1] == 6
 
     return white if color == WHITE
 
-    return [[1, 0], [2, 0]] unless @has_moved
+    return [[1, 0], [2, 0]] if @position[1] == 1
 
     [[1, 0]]
   end
