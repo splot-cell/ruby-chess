8ec5b3e9c5e74ad940860e9017a5bed061ca5728
Olly Seber <olly_s@hotmail.co.uk>
2022-12-14 15:41:14 -0800

    Remove pawn-specific code from base class

--- lib/piece.rb
+++ lib/piece.rb
@@ -31,10 +31,9 @@ class Piece
   end
 
   def possible_move_squares(board)
-    valid_moves = accessible_squares(move_translations, board).keep_if { |sq| board.square_empty?(sq) }
-    valid_attacks = threatened_squares(board).keep_if { |sq| board.color_at_sq(sq) == board.opponent_color(@color) }
-
-    (valid_moves + valid_attacks).uniq
+    accessible_squares(move_translations, board).keep_if do |sq|
+      board.square_empty?(sq) || board.color_at_sq(sq) == board.opponent_color(@color)
+    end
   end
 
   def threatened_squares(board)
