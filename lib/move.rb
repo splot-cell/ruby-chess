4c538bf9c0797e4ebd3b9f78e158411b9efbf076
Olly Seber <olly_s@hotmail.co.uk>
2022-12-14 21:24:12 -0800

    Update skeleton code to match current architecture
    Improve clarity of comments and remove any skeleton code that has been
    made redundant by recent work on Piece and Board classes

--- lib/move.rb
+++ lib/move.rb
@@ -5,21 +5,17 @@ require_relative "constants"
 class Move
   include PieceType
 
-  attr_accessor :en_passant, :castle, :promotion, :translation_list, :pawn_double_push, :piece
+  attr_accessor :promotion, :translation_list, :pawn_double_push, :piece
 
-  # checks whether it's a basic or complex move...
-  def basic?
-    !(en_passant || castle || promotion)
-  end
-
-  # update half move clk if necessary
-  # update full move num if necessary
-  # update en passant target if necessary
-  # update castlign if necessary
-  # call toggle_current_player
   def update_board_state(board)
-    board.en_passant_target = pawn_double_push ? self.en_passant_target : nil
+    # TO DO
+    # Update clocks here
 
+    # If the move is a pawn_double_push, set the board's en_passant_target,
+    # otherwise, set it to nil
+    board.en_passant_target = pawn_double_push ? en_passant_target : nil
+
+    # If a king or rook moves, remove the relevant castling flag from board
     board.remove_castling_avail(self) if piece.type == KING || piece.type == ROOK
 
     board.toggle_current_player
@@ -32,26 +28,23 @@ class Move
     board.translate_squares(translation_list)
   end
 
-  # checks whether move is valid on board
-  # check if en passant valid if necessary
-  # check if castling valid if necessary
-  # create version of board with new position and query in_check
   def valid?(board)
-    # if this is an en passant move, check that the square under attack is the en passant square
-    return false if en_passant && board.en_passant_target != translation_list[0][1]
-
-    return false if castle && board.castling_valid?(translation_list[1][0])
-
+    # Store the current board position
     current_state = board.encode_fen_position
-    self.execute(board)
-    ret = board.in_check?(board.current_player)
+    # Execute the proposed move
+    board.translate_squares(translation_list)
+    # Check whether the player is in check after the proposed move:
+    # if so, the move is not valid
+    ret = !board.in_check?(board.current_player)
+    # Restore the board position
     board.restore_position(current_state)
-
+    # Return the result of the validity check
     ret
   end
 
+  # Finds the square between the start square and finish square in a list of
+  # translations. For use with a pawn_double_push move
   def en_passant_target
     translation_list[0].reduce { |av, v| [(av[0] + v[0]) / 2, (av[1] + v[1]) / 2] }
   end
-
 end
