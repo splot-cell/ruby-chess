0dd70be2f45bdbb685ac4dfbdf95f91e9097449a
Olly Seber <olly_s@hotmail.co.uk>
2022-12-23 06:04:33 +0000

    Add tests for pawn Move creation

--- spec/move_spec.rb
+++ spec/move_spec.rb
@@ -3,4 +3,49 @@
 require_relative "../lib/move"
 
 describe Move do
+  describe "#initialize" do
+    let(:piece) { double("piece") }
+    let(:board) { double("board") }
+    subject(:move) { Move.new(piece, list, board) }
+
+    context "when the piece is a pawn" do
+      context "when the move is a single square push" do
+        let(:list) { [[6, 2], [5, 2]] }
+        before do
+          allow(piece).to receive(:type).and_return(PieceType::PAWN)
+          allow(piece).to receive(:color)
+          allow(board).to receive(:promotion_rank)
+        end
+
+        it "creates a Move object" do
+          expect(move).to be_a(Move)
+        end
+
+        it "creates an object with @pawn_double_push equal to false" do
+          expect(move.pawn_double_push).to eq(false)
+        end
+
+        it "creates an object with @promotion equal to false" do
+          expect(move.promotion).to eq(false)
+        end
+      end
+
+      context "when the move is a double square push" do
+        let(:list) { [[6, 4], [4, 4]] }
+        before do
+          allow(piece).to receive(:type).and_return(PieceType::PAWN)
+          allow(piece).to receive(:color)
+          allow(board).to receive(:promotion_rank)
+        end
+
+        it "creates an objet with @pawn_double_push equal to true" do
+          expect(move.pawn_double_push).to eq(true)
+        end
+
+        it "creates an object with @promotion equal to false" do
+          expect(move.promotion).to eq(false)
+        end
+      end
+    end
+  end
 end
