925a6904573858033381022b47b30421b1e62c7b
Olly Seber <olly_s@hotmail.co.uk>
2023-02-05 18:33:40 -0800

    Add methods for converting indexes to strings

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -8,4 +8,12 @@ module Coordinates
   def num_to_rank_index(char)
     8 - char.to_i
   end
+
+  def file_index_to_letter(index)
+    (index + 97).chr
+  end
+
+  def rank_index_to_number_str(index)
+    (8 - index).to_s
+  end
 end
