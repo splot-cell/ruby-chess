2a84d6b548bcc87d6e55c7bc4e861ba194d3beb6
Olly Seber <olly_s@hotmail.co.uk>
2023-02-23 14:25:06 -0800

    Use PieceTypes in move interpretor

--- lib/move_interpreter.rb
+++ lib/move_interpreter.rb
@@ -1,29 +1,46 @@
 # frozen_string_literal: true
 
 require_relative "constants"
+require_relative "coordinate_translator"
 
 class MoveInterpreter
-  def initialize(str)
+  include PieceType
+
+  def initialize(str, coordinate_translator = CoordinateTranslator.new)
     @move_data = str.match(/^([NBRQK])?([a-h])?([1-8])?([a-h][1-8])([NBRQ])?$/).captures
+    @coordinate_translator = coordinate_translator
   end
 
   def piece_type
-    @move_data[0]
+    interpret_type(@move_data[0])
   end
 
   def disambiguation_rank
-    @move_data[1]
+    @coordinate_translator.translate_rank(@move_data[1])
   end
 
   def disambiguation_file
-    @move_data[2]
+    @coordinate_translator.translate_file(@move_data[2])
   end
 
   def target_square
-    @move_data[3]
+    @coordinate_translator.translate_square(@move_data[3])
   end
 
   def promotion_value
-    @move_data[4]
+    @move_data[4].nil? ? nil : interpret_type(@move_data[4])
+  end
+
+  private
+
+  def interpret_type(char)
+    return PieceType::PAWN if char.nil?
+    {
+      "K" => PieceType::KING,
+      "Q" => PieceType::QUEEN,
+      "R" => PieceType::ROOK,
+      "B" => PieceType::BISHOP,
+      "N" => PieceType::KNIGHT,
+    }[char]
   end
 end
