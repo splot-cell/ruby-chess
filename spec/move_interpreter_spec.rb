88ab47475a7684ccd93b046ccd7e0757fbd1ff2a
Olly Seber <olly_s@hotmail.co.uk>
2023-02-23 18:47:31 -0800

    Refactor to streamline

--- spec/move_interpreter_spec.rb
+++ spec/move_interpreter_spec.rb
@@ -9,30 +9,25 @@ describe MoveInterpreter do
 
   describe "#interpret_move" do
     context "when the move is non-ambiguous" do
-      # let(:move) { move_interpreter.interpret_move("d3") }
-      let(:move_string) { "d3" }
+      let(:move) { move_interpreter.interpret_move("d3") }
       before do
         board.restore_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
       end
 
       it "returns a move with the correct piece" do
-        move = move_interpreter.interpret_move(move_string)
         piece = board.instance_variable_get(:@data)[6][3]
         expect(move.piece).to eq(piece)
       end
 
       it "returns a move of the correct promotion type" do
-        move = move_interpreter.interpret_move(move_string)
         expect(move.promotion).to be(false)
       end
 
       it "returns a move of the correct double push type" do
-        move = move_interpreter.interpret_move(move_string)
         expect(move.pawn_double_push).to be(false)
       end
 
       it "returns a move with the correct translation list" do
-        move = move_interpreter.interpret_move(move_string)
         expect(move.translation_list).to eq([[[6, 3], [5, 3]]])
       end
     end
