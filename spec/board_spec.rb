e3cef799749138beca68466cacc78c4b3f74ac60
Olly Seber <olly_s@hotmail.co.uk>
2023-02-23 18:44:51 -0800

    Move tests to move_interpreter_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -638,30 +638,4 @@ describe Board do
       end
     end
   end
-
-  describe "#interpret_move" do
-    context "when the move is non-ambiguous" do
-      let(:move) { board.interpret_move("d3") }
-      before do
-        board.restore_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-      end
-
-      it "returns a move with the correct piece" do
-        piece = board.instance_variable_get(:@data)[6][3]
-        expect(move.piece).to eq(piece)
-      end
-
-      it "returns a move of the correct promotion type" do
-        expect(move.promotion).to be(false)
-      end
-
-      it "returns a move of the correct double push type" do
-        expect(move.pawn_double_push).to be(false)
-      end
-
-      it "returns a move with the correct translation list" do
-        expect(move.translation_list).to eq([[[6, 3], [5, 3]]])
-      end
-    end
-  end
 end
