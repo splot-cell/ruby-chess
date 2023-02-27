c2ad1d46a892082cc5f04b9101876c361ac3f967
Olly Seber <olly_s@hotmail.co.uk>
2023-02-26 16:32:04 -0800

    Add serialization functionality

--- lib/game.rb
+++ lib/game.rb
@@ -4,15 +4,18 @@ require_relative "board"
 require_relative "constants"
 require_relative "move_interpreter"
 require_relative "game_text"
+require_relative "save_file_manager"
 
 class Game
   include Color
   include GameText
+  include SaveFileManager
 
   def initialize
     @board = Board.new
     @board.initialize_position
     @move_interpreter = MoveInterpreter.new(@board)
+    initialize_save_directory
   end
 
   def play
@@ -32,15 +35,11 @@ class Game
 
     return if choice == "1"
 
-    return load_savegame if choice == "2"
+    return load_game if choice == "2"
 
     puts "\nI'm sorry, I didn't understand that choice. Try again..."
 
     game_mode
-  end
-
-  def load_savegame
-    puts "Saving and loading is not currently supported"
   end
 
   def next_turn
@@ -48,7 +47,7 @@ class Game
     puts "\n#{@board}\n\n"
     puts "Check.\n\n" if @board.in_check?
 
-    move = @board.current_player == WHITE ? human_select_move : computer_select_move
+    move = @board.current_player == WHITE ? human_selection : computer_select_move
 
     move.execute
   end
@@ -59,16 +58,35 @@ class Game
     puts result
   end
 
-  def human_select_move
+  def human_selection
     print user_prompt
     choice = gets.chomp
+
+    save_game if choice.match(/^save$/i)
+
+    quit_game if choice.match(/^quit$/i)
+
+    human_select_move(choice)
+  end
+
+  def human_select_move(choice)
     # if the choice is in a recognized format
     move = @move_interpreter.interpret_move(choice) if @move_interpreter.move_format_valid?(choice)
     # if the board cannot identify the piece
     return move unless move.nil?
 
-    puts "I could not interpret that input. Type 'help' to see the instructions."
-    human_select_move
+    if choice.match(/^help$/i)
+      puts instructions
+    else
+      puts "I could not interpret that input. Type 'help' to see the instructions."
+    end
+    human_selection
+  end
+
+  def quit_game
+    puts "Quitting..."
+    sleep(1)
+    exit
   end
 
   def computer_select_move
---
+++ lib/save_file_manager.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+require "json"
+require_relative "save_file_manager_text"
+
+module SaveFileManager
+  include SaveFileManagerText
+
+  SAVES_DIR = "./saved_games/"
+
+  def initialize_save_directory
+    Dir.mkdir(SAVES_DIR) unless Dir.exist?(SAVES_DIR)
+  end
+
+  def save_game(data = @board.encode_game_state)
+    return too_many_saves if file_list.length >= total_unique_filenames
+
+    filename = generate_filename
+    File.open("#{SAVES_DIR}#{filename}", "w") do |file|
+      file.puts(JSON.dump(data))
+    end
+    puts game_saved_msg(filename)
+    exit
+  end
+
+  def load_game
+    return no_saves if file_list.empty?
+
+    filename = "#{SAVES_DIR}#{file_list[select_file]}"
+    data = JSON.load(File.read(filename))
+    File.delete(filename)
+    @board.restore_position(data)
+  end
+
+  def total_unique_filenames
+    filename_adjectives.length * filename_nouns.length
+  end
+
+  def filename_adjectives
+    %w[sleepy jumpy feisty humble proud]
+  end
+
+  def filename_nouns
+    %w[panda octopus lizard kangeroo lemur]
+  end
+
+  def generate_filename
+    filename = "#{filename_adjectives.sample}_#{filename_nouns.sample}.json"
+    return filename unless file_list.include?(filename)
+
+    generate_filename
+  end
+
+  def no_saves
+    puts no_save_files_msg
+    nil
+  end
+
+  def select_file
+    puts file_list_msg
+    selection = gets.chomp
+    return selection.to_i if selection.match(/^[0-9]+$/) &&
+                             selection.to_i < file_list.length
+
+    puts error_unrecognized_selection
+    select_file
+  end
+
+  def file_list
+    Dir.children(SAVES_DIR)
+  end
+
+  def too_many_saves
+    puts error_too_many_files
+  end
+end
